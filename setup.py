652a06cd7ecac51a1f8568098a2a2f32d7d8a454
Varun Reddy <amaravadhivarunreddy@gmail.com>
2026-02-04 22:39:41 +0530

    setup.py and req.txt

---
+++ setup.py
@@ -0,0 +1,25 @@
+## setup.py - will be responsible in creating my machine learning application as a package. and even deploy pypy
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    #the list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+setup(
+    name = "mlproject",
+    version="0.0.1",
+    author= "varun reddy",
+    author_email="amaravadhivarunreddy@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
